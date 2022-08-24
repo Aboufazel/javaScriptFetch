59c91fde1823d9cc47a60ab7ece436485614804d
Aboufazel <a.ab2521@gmail.com>
2022-08-24 21:57:47 +0430

    new table style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,23 @@
 
 
-const tabel = document.querySelector("#table")
+const tabel = document.querySelector("#table");
 
-const user = fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json()).then((users) => {
+
+
+const sample = fetch("https://jsonplaceholder.typicode.com/users").then( result => result.json()).then((sample) =>{
+    sample.forEach((sample , index ) => {
+        tabel.innerHTML += (`<tr>
+               <th scope="row" class="id_number">${sample.id}</th>
+               <td class="name">${sample.name}</td>
+               <td class="username">${sample.username}</td>
+               <td class="mail">${sample.email}</td>
+           </tr>
+`)
+    })
+});
+
+
+/*const user = fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json()).then((users) => {
     users.forEach((user , index) =>{
         tabel.innerHTML += (`<tr>
                <th scope="row" class="id_number">${user.id}</th>
@@ -11,4 +26,4 @@ const user = fetch("https://jsonplaceholder.typicode.com/users").then(res => res
                <td class="mail">${user.email}</td>
            </tr>`)
     })
-});
+});*/
